a10b234466bfdb28815a7045830ad514b25feb20
Alexei Lozovsky <me@ilammy.net>
2019-04-19 02:21:26 +0300

    Remove unnecessary example files
    We may need postinst and postrm scripts later, after the package split,
    but for now they are unnecessary. Drop them along with all other stuff
    that dh_make put there for us.

--- debian/chibi-scheme.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the chibi-scheme package
-#
-0 4	* * *	root	[ -x /usr/bin/chibi-scheme_maintenance ] && /usr/bin/chibi-scheme_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(chibi-scheme):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="chibi-scheme" command="/usr/bin/chibi-scheme"
